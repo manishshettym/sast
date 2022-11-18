c8ed82bd38d22075a54663856d0f671fbf8aecca
manishshettym <manishs@berkeley.edu>
2022-11-18 10:44:25 -0800

    adding a package setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simplipy',
+    version='0.0.0',
+    packages=find_packages(where='.'),
+    url='https://github.com/manishshettm/simplipy',
+    license='BSD-2-Clause',
+    author='Manish Shetty',
+    author_email='manishs@berkeley.edu',
+    description='Your new favourite program representation'
+)
